2f4f2c22beeaba98c1510402ce3386d187f121b1
viniljose <vinilpj@gmail.com>
2021-11-17 17:15:22 +0530

    Update App.kt

--- src/com/vinil/kotlin/App.kt
+++ src/com/vinil/kotlin/App.kt
@@ -15,4 +15,11 @@ fun main(args :Array<String>) {
     var z: Short = 6
     //var x: Int = 3.12
     var k: Long = z.toLong()
+    
+    println()
+    println(if(x>y) "x is greater than y" else "x is less than y ")
+    println("-------------")
+
+    var myArray = arrayOf(1,2,3)
+    myArray.forEach { a -> println(a) }
 }
