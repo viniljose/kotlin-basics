1c62e3b58ee8ed9446ca8166cacdf221b072fe4d
viniljose <vinilpj@gmail.com>
2021-11-17 14:49:30 +0530

    Create App.kt

---
+++ src/com/vinil/kotlin/App.kt
@@ -0,0 +1,18 @@
+package com.vinil.kotlin
+
+fun main(args :Array<String>) {
+    println("Hello World")
+    var x = 3
+    val name = "Vinil"
+    x = x*10
+    print("x is $x")
+    while(x>20){
+        x=x-1
+        print("x is now $x")
+
+    }
+    var smallNum :Byte
+    var z: Short = 6
+    //var x: Int = 3.12
+    var k: Long = z.toLong()
+}
